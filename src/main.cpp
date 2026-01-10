632cccbeb299d54628dffe96c35fa8a1e7a71ae9
Hakatatanoodle <literallyme098@gmail.com>
2026-01-10 17:33:47 +0545

    update assignment and delete subject added

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,23 @@ struct Subject
   std::string name;
   std::vector<Assignment> assignments;
 };
+struct Index
+{
+  int subjectIndex;
+  int assignmentIndex;
+
+};
 //function prototypes
 void addSubject(std::vector<Subject>& subjects);
 void addAssignment(std::vector<Subject>& subjects);
 void viewAssignments(std::vector<Subject>& subjects);
 void updateAssignment(std::vector<Subject>& subjects);
 void deleteAssignment();
+void deleteSubject(std::vector<Subject>& subjects);
 void saveAndExit();
 int chooseSubject(std::vector<Subject>& subjects);
-int chooseAssignment(std::vector<Subject>& subjects);
+Index chooseAssignment(std::vector<Subject>& subjects);
+int inputValidation(std::string);
 
 int main()
 {
@@ -37,7 +45,8 @@ int main()
   std::cout<<"3. View Assignments\n";
   std::cout<<"4. Update Assignment\n";
   std::cout<<"5. Delete Assignment\n";
-  std::cout<<"6. Save and Exit\n";
+  std::cout<<"6. Delete Subject\n";
+  std::cout<<"7. Save and Exit\n";
   std::cout<<"choose: ";
   std::cin>>choice;
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -58,17 +67,37 @@ int main()
     case 5:
     deleteAssignment();
     break;
-    case 6:
+    case 6: 
+    deleteSubject(subjects);
+    break;
+    case 7:
     saveAndExit();
     break;
     default:
     std::cout<<"Invalid choice\n";
     break;
   }
-}while(choice!=6);
+}while(choice!=7);
   return 0;
 }
 //function definitions
+int inputValidation(std::string s)
+{
+  int index;
+  try
+  {
+    {
+      // inputValidation converts 1-based input to 0-based index
+      index = std::stoi(s) - 1;
+    }
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return -1 ;
+  }
+  return index; 
+}
 int chooseSubject(std::vector<Subject>& subjects)
 {
     if (subjects.empty())
@@ -87,15 +116,7 @@ int chooseSubject(std::vector<Subject>& subjects)
     std::getline(std::cin >> std::ws, input);
 
     int subIndex;
-    try
-    {
-        subIndex = std::stoi(input) - 1;
-    }
-    catch (...)
-    {
-        std::cout << "Invalid input!";
-        return -1;
-    }
+    subIndex = inputValidation(input);
 
     if (subIndex < 0 || subIndex >= subjects.size())
     {
@@ -105,18 +126,18 @@ int chooseSubject(std::vector<Subject>& subjects)
 
     return subIndex;
 }
-int chooseAssignment(std::vector<Subject>& subjects)
+Index chooseAssignment(std::vector<Subject>& subjects)
 {
   int subIndex = chooseSubject(subjects);
   if(subIndex == -1)
   {
     std::cout<<"Indexing error!";
-    return -1;
+    return Index{-1,-1};
   }
   if(subjects[subIndex].assignments.size()<=0) 
   {
     std::cout<<" NO assignments for this subject!"; 
-    return -1;
+    return Index{-1,-1};
   }
   std::cout << "Choose the assignment: " << std::endl;
   for(size_t i=0; i<subjects[subIndex].assignments.size();i++)
@@ -128,21 +149,21 @@ int chooseAssignment(std::vector<Subject>& subjects)
   int assIndex;
     try
     {
-        assIndex = std::stoi(aInput) - 1;
+        assIndex = std::stoi(aInput)-1;
     }
     catch (...)
     {
-        std::cout << "Invalid input!";
-        return -1;
+        std::cout << "Invalid input!"<<std::endl;
+        return Index{-1,-1};
     }
 
     if (assIndex < 0 || assIndex >= subjects[subIndex].assignments.size())
     {
-        std::cout << "Out of range!";
-        return -1;
+        std::cout << "Out of range!"<<std::endl;
+        return Index{-1,-1};
     }
 
-    return assIndex;
+    return Index{subIndex,assIndex};
 }
 
 void addSubject(std::vector<Subject>& subjects)
@@ -198,11 +219,11 @@ void viewAssignments(std::vector<Subject>& subjects)
 }
 void updateAssignment(std::vector<Subject>& subjects)
 {
-  int assignmentIndex;
-  assignmentIndex = chooseAssignment(subjects);
-  if( assignmentIndex == -1 )
+  Index assubIndex;
+  assubIndex = chooseAssignment(subjects);
+  if( assubIndex.subjectIndex == -1 || assubIndex.assignmentIndex == -1 )
   {
-    std::cout<<"Indexing error!";
+    std::cout<<"Indexing error!"<<std::endl;
     return ;
   }
 
@@ -213,15 +234,63 @@ void updateAssignment(std::vector<Subject>& subjects)
   std::cout << "4.Assignment status" << std::endl;
 
   std::string aChoice;
-  getline( std::cin, aChoice );
+  std::getline( std::cin, aChoice );
+  int choice;
+  choice = inputValidation(aChoice);
+  std::cout<<choice;
+  // inputValidation converts 1-based input to 0-based index
 
-
-
-
+  if(choice == 0)
+  {
+    std::cout << "Current title : " << subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].title << std::endl;
+    std::cout << "Enter new title : ";
+    std::getline(std::cin>>std::ws,subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].title);
+  }
+  else if(choice == 1)
+  {
+    std::cout << "Current description : " << subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].description << std::endl;
+    std::cout << "Enter new description : ";
+    std::getline(std::cin>>std::ws,subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].description);
+  }
+  else if(choice == 2)
+  {
+    std::cout << "Current deadline : " << subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].deadline << std::endl;
+    std::cout << "Enter new deadline : " ;
+    std::getline(std::cin>>std::ws,subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].deadline);
+  }
+  else if(choice == 3)
+  {
+    std::cout << "current status : " << subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].status << std::endl;
+    std::cout << "Toggle (Done/NotDone) : " << std::endl;
+    std::string statusInput;
+    std::getline(std::cin >> std::ws ,statusInput);
+    if(statusInput == "Done" || statusInput == "done" || statusInput == "Yes" || statusInput == "yes")
+    {
+      subjects[assubIndex.subjectIndex].assignments[assubIndex.assignmentIndex].status = true;
+    }
+    else if(statusInput == "not done" || statusInput == "no")
+    {
+      statusInput = false;
+    }
+    else{
+      std::cout << " invalid choice!"<<std::endl;
+      return ;
+    }
+  }
 }
+
 void deleteAssignment()
 {
-  std::cout<<"Delete Assignment\n"<<std::endl;
+  
+}
+void deleteSubject(std::vector<Subject>& subjects)
+{
+  std::cout<<"Choose Subject to delete: " << std::endl;
+  int dChoose;
+  dChoose = chooseSubject(subjects);
+  std::cout<<"Subject : "<< subjects[dChoose].name << " deleted!" << std::endl;
+  subjects.erase(subjects.begin() + dChoose);
+
 }
 void saveAndExit()
 {
